3fc33397a18dbd43045600ae428fb790e85400ba
phucbinh2001 <48088066+phucbinh2001@users.noreply.github.com>
2020-11-06 15:08:50 +0700

    Code layout

--- sinhvien/views/layout.php
+++ sinhvien/views/layout.php
@@ -1,6 +1,7 @@
 <!-- chứa layout phan sinh vien -->
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -14,29 +15,65 @@
     <script src="../system/jsvali.js"></script>
     <script src="./system/icons.js"></script>
     <!-- bootstrap -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+        integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
+        integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js"
+        integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous">
+    </script>
     <!-- end bootstrap -->
     <title>Sinh Viên</title>
 </head>
+
 <body>
-<div class="menu">
-    <a href="index.php?act=baitap">Bài Tập</a>
-    <a href="index.php?act=dkkh">đăng ký khóa học</a>
-    <a href="index.php?act=thongbao">thongbao</a>
-</div>
+    <div class="container-fluid h-100 p-0">
+        <div class="sidebar pt-5">
+            <div class="account text-light ml-4 mb-5">
+                <img class="avatar mr-3" src="./views/img/avatar.jpg" alt="">
+                <div class="info">
+                    <strong>
+                        <p class="mb-1">Bình Phúc</p>
+                    </strong>
+                    <span class="idsv">PS15313</span>
+                </div>
+            </div>
+            <div class="menu m-3">
+                <ul>
+                    <li class="active"><a href="#">Tổng quan</a><i class="uim uim-panel-add"></i></li>
+                    <li><a href="index.php?act=baitap">Bài Tập</a></li>
+                    <li><a href="#">Bảng điểm</a></li>
+                    <li><a href="index.php?act=dkkh">Khóa học</a></li>
+                    <li><a href="index.php?act=thongbao">Thông báo</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="box-right">
+            <!-- tiêu dề đặt trên đầu mỗi trang view cần tiêu đề -->
+            <div class="header-box">
+                <div class="tieude h1">Bài tập</div>
+                <div class="option-box">
+                    <a href="#" class="active">Tất cả</a>
+                    <a href="#">Đã nộp</a>
+                    <a href="#">Chưa nộp</a>
+                </div>
+            </div>
+
+            <!-- bình chia layout xong khóa lại ai cần thì copy vào page mình nha -->
 
 
-<!-- tiêu dề đặt trên đầu mỗi trang view cần tiêu đề -->
-<div class="tieude"></div> 
-<!-- bình chia layout xong khóa lại ai cần thì copy vào page mình nha -->
+            <div class="noidung">
+                <?php require_once $view?>
+            </div>
+        </div>
 
 
-<div class="noidung">
-<?php require_once $view?>
-</div>
 
+    </div>
 </body>
+
 </html>
