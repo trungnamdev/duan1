3b18fafc9e63794f4ecc7cef9e5f4ae488387286
quangdatcqd <73808699+quangdatcqd@users.noreply.github.com>
2020-11-07 23:31:58 +0700

    loi css

--- sinhvien/views/baitap.php
+++ sinhvien/views/baitap.php
@@ -1,254 +1,254 @@
 <div class="header-box">
-                <div class="tieude h1">BẢNG ĐIỂM</div>
-                <div class="option-box">
-                    <a href="#" class="active">Tất cả</a>
-                    <a href="#">Đã nộp</a>
-                    <a href="#">Chưa nộp</a>
-                </div>
+    <div class="tieude h1">BẢNG ĐIỂM</div>
+    <div class="option-box">
+        <a href="#" class="active">Tất cả</a>
+        <a href="#">Đã nộp</a>
+        <a href="#">Chưa nộp</a>
+    </div>
 </div>
-   <div class="d-row">
-   <div class="d-div3">
-           <div class="d-div3-img">
-           <img src="./views/img/baitap.png" alt="">
-           </div>
-           <br>
-           <div class="d-info">
-               <div class="d-row100">
-                   
-             
-               <div class="d-info1">
-                   <img src="./views/img/baitap.png" alt="">
-                   <a href="">Mr.Hào LT</a>
-               </div>
-               <div class="d-info2">
-                  PHP CƠ BẢN
-               </div>
-           </div>
-           </div>
-           <div class="d-info">
-               <div class="d-row1">
-               <p>Giai đoạn 2: <br>
-Thiết kế database</p>
-<span>Our team was inspired by the seven skills of highly effective programmers created by
-     the TechLead. We wanted to provide our own take on the topic...</span>
+<div class="d-row">
+    <div class="d-div3">
+        <div class="d-div3-img">
+            <img src="./views/img/baitap.png" alt="">
+        </div>
+        <br>
+        <div class="d-info">
+            <div class="d-row100">
 
-               </div>
-           </div>
-           <div class="d-info">
-               <div class="d-row100  mb-18">
-                   
-             
-               <div class="d-info1 d-hc">
-                   <a href="">Hạn chót: 10/11/2020 </a>
-               </div>
-               <div class="d-info2 d-nb">
-                 <a href="">Nộp bài</a>
-               </div>
-           </div>
-           </div>
-       </div>
-       <div class="d-div3">
-           <div class="d-div3-img">
-           <img src="./views/img/baitap.png" alt="">
-           </div>
-           <br>
-           <div class="d-info">
-               <div class="d-row100">
-                   
-             
-               <div class="d-info1">
-                   <img src="./views/img/baitap.png" alt="">
-                   <a href="">Mr.Hào LT</a>
-               </div>
-               <div class="d-info2">
-                  PHP CƠ BẢN
-               </div>
-           </div>
-           </div>
-           <div class="d-info">
-               <div class="d-row1">
-               <p>Giai đoạn 2: <br>
-Thiết kế database</p>
-<span>Our team was inspired by the seven skills of highly effective programmers created by
-     the TechLead. We wanted to provide our own take on the topic...</span>
 
-               </div>
-           </div>
-           <div class="d-info">
-               <div class="d-row100  mb-18">
-                   
-             
-               <div class="d-info1 d-hc">
-                   <a href="">Hạn chót: 10/11/2020 </a>
-               </div>
-               <div class="d-info2 d-nb">
-                 <a href="">Nộp bài</a>
-               </div>
-           </div>
-           </div>
-       </div>
-       <div class="d-div3">
-           <div class="d-div3-img">
-           <img src="./views/img/baitap.png" alt="">
-           </div>
-           <br>
-           <div class="d-info">
-               <div class="d-row100">
-                   
-             
-               <div class="d-info1">
-                   <img src="./views/img/baitap.png" alt="">
-                   <a href="">Mr.Hào LT</a>
-               </div>
-               <div class="d-info2">
-                  PHP CƠ BẢN
-               </div>
-           </div>
-           </div>
-           <div class="d-info">
-               <div class="d-row1">
-               <p>Giai đoạn 2: <br>
-Thiết kế database</p>
-<span>Our team was inspired by the seven skills of highly effective programmers created by
-     the TechLead. We wanted to provide our own take on the topic...</span>
+                <div class="d-info1">
+                    <img src="./views/img/baitap.png" alt="">
+                    <a href="">Mr.Hào LT</a>
+                </div>
+                <div class="d-info2">
+                    PHP CƠ BẢN
+                </div>
+            </div>
+        </div>
+        <div class="d-info">
+            <div class="d-row1">
+                <p>Giai đoạn 2: <br>
+                    Thiết kế database</p>
+                <span>Our team was inspired by the seven skills of highly effective programmers created by
+                    the TechLead. We wanted to provide our own take on the topic...</span>
 
-               </div>
-           </div>
-           <div class="d-info">
-               <div class="d-row100  mb-18">
-                   
-             
-               <div class="d-info1 d-hc">
-                   <a href="">Hạn chót: 10/11/2020 </a>
-               </div>
-               <div class="d-info2 d-nb">
-                 <a href="">Nộp bài</a>
-               </div>
-           </div>
-           </div>
-       </div>
+            </div>
+        </div>
+        <div class="d-info">
+            <div class="d-row100  mb-18">
 
-       <div class="d-div3">
-           <div class="d-div3-img">
-           <img src="./views/img/baitap.png" alt="">
-           </div>
-           <br>
-           <div class="d-info">
-               <div class="d-row100">
-                   
-             
-               <div class="d-info1">
-                   <img src="./views/img/baitap.png" alt="">
-                   <a href="">Mr.Hào LT</a>
-               </div>
-               <div class="d-info2">
-                  PHP CƠ BẢN
-               </div>
-           </div>
-           </div>
-           <div class="d-info">
-               <div class="d-row1">
-               <p>Giai đoạn 2: <br>
-Thiết kế database</p>
-<span>Our team was inspired by the seven skills of highly effective programmers created by
-     the TechLead. We wanted to provide our own take on the topic...</span>
 
-               </div>
-           </div>
-           <div class="d-info">
-               <div class="d-row100  mb-18">
-                   
-             
-               <div class="d-info1 d-hc">
-                   <a href="">Hạn chót: 10/11/2020 </a>
-               </div>
-               <div class="d-info2 d-dn">
-                 <a href="">Đã nộp</a>
-               </div>
-           </div>
-           </div>
-       </div>
-       
-       <div class="d-div3">
-           <div class="d-div3-img">
-           <img src="./views/img/baitap.png" alt="">
-           </div>
-           <br>
-           <div class="d-info">
-               <div class="d-row100">
-                   
-             
-               <div class="d-info1">
-                   <img src="./views/img/baitap.png" alt="">
-                   <a href="">Mr.Hào LT</a>
-               </div>
-               <div class="d-info2">
-                  PHP CƠ BẢN
-               </div>
-           </div>
-           </div>
-           <div class="d-info">
-               <div class="d-row1">
-               <p>Giai đoạn 2: <br>
-Thiết kế database</p>
-<span>Our team was inspired by the seven skills of highly effective programmers created by
-     the TechLead. We wanted to provide our own take on the topic...</span>
+                <div class="d-info1 d-hc">
+                    <a href="">Hạn chót: 10/11/2020 </a>
+                </div>
+                <div class="d-info2 d-nb">
+                    <a href="">Nộp bài</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="d-div3">
+        <div class="d-div3-img">
+            <img src="./views/img/baitap.png" alt="">
+        </div>
+        <br>
+        <div class="d-info">
+            <div class="d-row100">
 
-               </div>
-           </div>
-           <div class="d-info">
-               <div class="d-row100  mb-18">
-                   
-             
-               <div class="d-info1 d-hc">
-                   <a href="">Hạn chót: 10/11/2020 </a>
-               </div>
-               <div class="d-info2 d-dn">
-                 <a href="">Đã chấm: 9/10</a>
-               </div>
-           </div>
-           </div>
-       </div>
-       
-       <div class="d-div3">
-           <div class="d-div3-img">
-           <img src="./views/img/baitap.png" alt="">
-           </div>
-           <br>
-           <div class="d-info">
-               <div class="d-row100">
-                   
-             
-               <div class="d-info1">
-                   <img src="./views/img/baitap.png" alt="">
-                   <a href="">Mr.Hào LT</a>
-               </div>
-               <div class="d-info2">
-                  PHP CƠ BẢN
-               </div>
-           </div>
-           </div>
-           <div class="d-info">
-               <div class="d-row1">
-               <p>Giai đoạn 2: <br>
-Thiết kế database</p>
-<span>Our team was inspired by the seven skills of highly effective programmers created by
-     the TechLead. We wanted to provide our own take on the topic...</span>
 
-               </div>
-           </div>
-           <div class="d-info">
-               <div class="d-row100  mb-18">
-                   
-             
-               <div class="d-info1 d-hc">
-                   <a href="">Hạn chót: 10/11/2020 </a>
-               </div>
-               <div class="d-info2 d-nb">
-                 <a href="">Nộp bài</a>
-               </div>
-           </div>
-           </div>
-       </div>
-       
-   </div>
+                <div class="d-info1">
+                    <img src="./views/img/baitap.png" alt="">
+                    <a href="">Mr.Hào LT</a>
+                </div>
+                <div class="d-info2">
+                    PHP CƠ BẢN
+                </div>
+            </div>
+        </div>
+        <div class="d-info">
+            <div class="d-row1">
+                <p>Giai đoạn 2: <br>
+                    Thiết kế database</p>
+                <span>Our team was inspired by the seven skills of highly effective programmers created by
+                    the TechLead. We wanted to provide our own take on the topic...</span>
+
+            </div>
+        </div>
+        <div class="d-info">
+            <div class="d-row100  mb-18">
+
+
+                <div class="d-info1 d-hc">
+                    <a href="">Hạn chót: 10/11/2020 </a>
+                </div>
+                <div class="d-info2 d-nb">
+                    <a href="">Nộp bài</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="d-div3">
+        <div class="d-div3-img">
+            <img src="./views/img/baitap.png" alt="">
+        </div>
+        <br>
+        <div class="d-info">
+            <div class="d-row100">
+
+
+                <div class="d-info1">
+                    <img src="./views/img/baitap.png" alt="">
+                    <a href="">Mr.Hào LT</a>
+                </div>
+                <div class="d-info2">
+                    PHP CƠ BẢN
+                </div>
+            </div>
+        </div>
+        <div class="d-info">
+            <div class="d-row1">
+                <p>Giai đoạn 2: <br>
+                    Thiết kế database</p>
+                <span>Our team was inspired by the seven skills of highly effective programmers created by
+                    the TechLead. We wanted to provide our own take on the topic...</span>
+
+            </div>
+        </div>
+        <div class="d-info">
+            <div class="d-row100  mb-18">
+
+
+                <div class="d-info1 d-hc">
+                    <a href="">Hạn chót: 10/11/2020 </a>
+                </div>
+                <div class="d-info2 d-nb">
+                    <a href="">Nộp bài</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="d-div3">
+        <div class="d-div3-img">
+            <img src="./views/img/baitap.png" alt="">
+        </div>
+        <br>
+        <div class="d-info">
+            <div class="d-row100">
+
+
+                <div class="d-info1">
+                    <img src="./views/img/baitap.png" alt="">
+                    <a href="">Mr.Hào LT</a>
+                </div>
+                <div class="d-info2">
+                    PHP CƠ BẢN
+                </div>
+            </div>
+        </div>
+        <div class="d-info">
+            <div class="d-row1">
+                <p>Giai đoạn 2: <br>
+                    Thiết kế database</p>
+                <span>Our team was inspired by the seven skills of highly effective programmers created by
+                    the TechLead. We wanted to provide our own take on the topic...</span>
+
+            </div>
+        </div>
+        <div class="d-info">
+            <div class="d-row100  mb-18">
+
+
+                <div class="d-info1 d-hc">
+                    <a href="">Hạn chót: 10/11/2020 </a>
+                </div>
+                <div class="d-info2 d-dn">
+                    <a href="">Đã nộp</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="d-div3">
+        <div class="d-div3-img">
+            <img src="./views/img/baitap.png" alt="">
+        </div>
+        <br>
+        <div class="d-info">
+            <div class="d-row100">
+
+
+                <div class="d-info1">
+                    <img src="./views/img/baitap.png" alt="">
+                    <a href="">Mr.Hào LT</a>
+                </div>
+                <div class="d-info2">
+                    PHP CƠ BẢN
+                </div>
+            </div>
+        </div>
+        <div class="d-info">
+            <div class="d-row1">
+                <p>Giai đoạn 2: <br>
+                    Thiết kế database</p>
+                <span>Our team was inspired by the seven skills of highly effective programmers created by
+                    the TechLead. We wanted to provide our own take on the topic...</span>
+
+            </div>
+        </div>
+        <div class="d-info">
+            <div class="d-row100  mb-18">
+
+
+                <div class="d-info1 d-hc">
+                    <a href="">Hạn chót: 10/11/2020 </a>
+                </div>
+                <div class="d-info2 d-dn">
+                    <a href="">Đã chấm: 9/10</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="d-div3">
+        <div class="d-div3-img">
+            <img src="./views/img/baitap.png" alt="">
+        </div>
+        <br>
+        <div class="d-info">
+            <div class="d-row100">
+
+
+                <div class="d-info1">
+                    <img src="./views/img/baitap.png" alt="">
+                    <a href="">Mr.Hào LT</a>
+                </div>
+                <div class="d-info2">
+                    PHP CƠ BẢN
+                </div>
+            </div>
+        </div>
+        <div class="d-info">
+            <div class="d-row1">
+                <p>Giai đoạn 2: <br>
+                    Thiết kế database</p>
+                <span>Our team was inspired by the seven skills of highly effective programmers created by
+                    the TechLead. We wanted to provide our own take on the topic...</span>
+
+            </div>
+        </div>
+        <div class="d-info">
+            <div class="d-row100  mb-18">
+
+
+                <div class="d-info1 d-hc">
+                    <a href="">Hạn chót: 10/11/2020 </a>
+                </div>
+                <div class="d-info2 d-nb">
+                    <a href="">Nộp bài</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
