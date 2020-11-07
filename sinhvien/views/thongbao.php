257e27936fbdc40b7a90ca6d5ea024fa1d7d66e6
Hoang Nguyen <hoangdean01@gmail.com>
2020-11-08 00:26:46 +0700

    master

--- sinhvien/views/thongbao.php
+++ sinhvien/views/thongbao.php
@@ -1,3 +1,168 @@
-<div class="header-box">
-    <div class="tieude h1">THÔNG BÁO</div>
-</div>
+<div class="w-100">
+    <div class="boxthongbao-left">
+        <div class="header-box">
+            <div class="tieude h1">THÔNG BÁO</div>
+        </div>
+
+        <div class="thongbao mg-top70">
+            <div class="boxthongbao">
+                <div class="item mb-3">
+                    <a href="#" class="tieude-tb">Về việc hoàn thành học phí kì Fall 2020</a>
+                    <div class="text-secondary info mt-2">
+                        <div class="mr-4">
+                            <i class="uim uim-user-nurse "></i>
+                            <span class="ml-1">Khải Hoàng</span>
+                        </div>
+                        <div>
+                            <i class="uim uim-clock"></i>
+                            <span class="ml-1">20/10/2020</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <hr>
+
+            <div class="boxthongbao">
+                <div class="item mb-3">
+                    <a href="#" class="tieude-tb">Về việc hoàn thành học phí kì Fall 2020</a>
+                    <div class="text-secondary info mt-2">
+                        <div class="mr-4">
+                            <i class="uim uim-user-nurse "></i>
+                            <span class="ml-1">Khải Hoàng</span>
+                        </div>
+                        <div>
+                            <i class="uim uim-clock"></i>
+                            <span class="ml-1">20/10/2020</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <hr>
+
+            <div class="boxthongbao">
+                <div class="item mb-3">
+                    <a href="#" class="tieude-tb">Về việc hoàn thành học phí kì Fall 2020</a>
+                    <div class="text-secondary info mt-2">
+                        <div class="mr-4">
+                            <i class="uim uim-user-nurse "></i>
+                            <span class="ml-1">Khải Hoàng</span>
+                        </div>
+                        <div>
+                            <i class="uim uim-clock"></i>
+                            <span class="ml-1">20/10/2020</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <hr>
+
+            <div class="boxthongbao">
+                <div class="item mb-3">
+                    <a href="#" class="tieude-tb">Về việc hoàn thành học phí kì Fall 2020</a>
+                    <div class="text-secondary info mt-2">
+                        <div class="mr-4">
+                            <i class="uim uim-user-nurse "></i>
+                            <span class="ml-1">Khải Hoàng</span>
+                        </div>
+                        <div>
+                            <i class="uim uim-clock"></i>
+                            <span class="ml-1">20/10/2020</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <hr>
+
+            <div class="boxthongbao">
+                <div class="item mb-3">
+                    <a href="#" class="tieude-tb">Về việc hoàn thành học phí kì Fall 2020</a>
+                    <div class="text-secondary info mt-2">
+                        <div class="mr-4">
+                            <i class="uim uim-user-nurse "></i>
+                            <span class="ml-1">Khải Hoàng</span>
+                        </div>
+                        <div>
+                            <i class="uim uim-clock"></i>
+                            <span class="ml-1">20/10/2020</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <hr>
+
+            <div class="boxthongbao">
+                <div class="item mb-3">
+                    <a href="#" class="tieude-tb">Về việc hoàn thành học phí kì Fall 2020</a>
+                    <div class="text-secondary info mt-2">
+                        <div class="mr-4">
+                            <i class="uim uim-user-nurse "></i>
+                            <span class="ml-1">Khải Hoàng</span>
+                        </div>
+                        <div>
+                            <i class="uim uim-clock"></i>
+                            <span class="ml-1">20/10/2020</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <hr>
+
+            <div class="boxthongbao">
+                <div class="item mb-3">
+                    <a href="#" class="tieude-tb">Về việc hoàn thành học phí kì Fall 2020</a>
+                    <div class="text-secondary info mt-2">
+                        <div class="mr-4">
+                            <i class="uim uim-user-nurse "></i>
+                            <span class="ml-1">Khải Hoàng</span>
+                        </div>
+                        <div>
+                            <i class="uim uim-clock"></i>
+                            <span class="ml-1">20/10/2020</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+
+        </div>
+    </div>
+
+    <div class="boxthongbao-right">
+        <div class="boxthongbao-chitiet">
+            <i class="fas fa-stream"></i>
+            <p class="text-secondary">Chọn một thông báo để xem</p>
+        </div>
+
+    </div>
+
+
+
+
+
+
+
+
+    <!-- Sử dụng câu lệnh for
+<?php
+for ($i = 0; $i < 6; $i++) {
+    echo  '
+       <div class="thongbao">
+           <div class="boxthongbao">
+               <div class="item mb-3">
+                   <a href="#" class="tieude-tb">Về việc hoàn thành học phí kì Fall 2020</a>
+                   <div class="info mt-2">
+                       <div class="mr-4">
+                           <i class="uim uim-user-nurse "></i>
+                           <span class="ml-1">Khải Hoàng</span>
+                       </div>
+                       <div>
+                           <i class="uim uim-clock"></i>
+                           <span class="ml-1">20/10/2020</span>
+                       </div>
+                   </div>
+               </div>
+           </div>
+       </div> ';
+}
+?> -->
