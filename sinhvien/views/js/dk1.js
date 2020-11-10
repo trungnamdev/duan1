d5ca22bf73472d792ffd7b64f0aeb8284da23c0b
Nam Nguyen <namn71201@gmail.com>
2020-11-10 23:12:56 +0700

    xoa file js

--- sinhvien/views/js/dk1.js
+++ sinhvien/views/js/dk1.js
@@ -2,4 +2,16 @@ $(document).ready(function() {
     $("#baitap").change(function() {
         $('#btshow').html($(this).val().split('\\').pop());
     });
+    $(".dkkh").click(function() {
+        var a = $(this).parent().children()[1].childNodes[3].value;
+        console.log(a);
+        $.ajax({
+            method: "POST", // phương thức dữ liệu được truyền đi
+            url: "index.php?act=dkkh1", // gọi đến file server show_data.php để xử lý
+            data: { id: a }, //lấy toàn thông tin các fields trong form bằng hàm serialize của jquery
+            success: function(data) { //kết quả trả về từ server nếu gửi thành công
+                alert("BẠN ĐÃ ĐĂNG KÍ THÀNH CÔNG KHÓA HỌC,MONG SINH VIÊN HỌC ĐẦY ĐỦ ĐỂ THÀNH CÔNG")
+            }
+        });
+    });
 });
