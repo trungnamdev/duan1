0b64cee20036b2541e8d0e1513c704e086c5a01e
Nam Nguyen <namn71201@gmail.com>
2020-11-11 20:43:46 +0700

    do lam van nhu db

--- sinhvien/views/js/dk1.js
+++ sinhvien/views/js/dk1.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
             url: "index.php?act=dkkh1", // gọi đến file server show_data.php để xử lý
             data: { id: a }, //lấy toàn thông tin các fields trong form bằng hàm serialize của jquery
             success: function(data) { //kết quả trả về từ server nếu gửi thành công
-                alert("BẠN ĐÃ ĐĂNG KÍ THÀNH CÔNG KHÓA HỌC,MONG SINH VIÊN HỌC ĐẦY ĐỦ ĐỂ THÀNH CÔNG")
+                alert("ĐĂNG KÝ THÀNH CÔNG");
                 location.reload();
             }
         });
