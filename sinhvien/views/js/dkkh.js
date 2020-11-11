b17ab776065f52244183c8373bf57712459e9cf1
thanhdo21 <73809018+thanhdo21@users.noreply.github.com>
2020-11-11 10:10:10 +0700

    code lai

--- sinhvien/views/js/dkkh.js
+++ sinhvien/views/js/dkkh.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     var submit = $(".dkkh");
     submit.click(function() {
-        var a = $(this).parent().children()[1].childNodes[3].value;
+        var a = $(this).parent().children()[1].childNodes[1].value;
         console.log(a);
         $.ajax({
             method: "POST", // phương thức dữ liệu được truyền đi
@@ -9,7 +9,7 @@ $(document).ready(function() {
             data: { id: a }, //lấy toàn thông tin các fields trong form bằng hàm serialize của jquery
             success: function(data) { //kết quả trả về từ server nếu gửi thành công
                 alert("BẠN ĐÃ ĐĂNG KÍ THÀNH CÔNG KHÓA HỌC,MONG SINH VIÊN HỌC ĐẦY ĐỦ ĐỂ THÀNH CÔNG")
-
+                location.reload();
 
 
             }
