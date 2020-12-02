2a05f24b99804788fe0a7129c46519b558b5eae7
Nam Nguyen <namn71201@gmail.com>
2020-12-02 15:47:02 +0700

    them sua font

--- system/conn.php
+++ system/conn.php
@@ -9,6 +9,7 @@ function ketnoidb()
         $conn = new PDO("mysql:host=$servername;dbname=duan1", $username, $password);
 
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        
         return $conn;
     } catch (PDOException $e) {
     }
